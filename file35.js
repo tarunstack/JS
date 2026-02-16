93211753566811a702f6e60848271f08301ab2ab
Tarun Swami <152185804+tarunstack@users.noreply.github.com>
2026-02-16 11:19:50 +0530

    file35.js
    methods imp

---
+++ file35.js
@@ -0,0 +1,38 @@
+// const names = ["Surya","Ajay","Sumanth","Satyamm"]
+// // names.push("Vivek") // adds at last
+// // names.pop()// removes last item in that array
+
+// names.shift()// removes first one in that array
+// names.unshift("Vivek")// adds at first in that array
+
+// // shift and unshift are slower due to rearrangement req 
+// console.log(names);
+// const names = new Array("Surya", "Ajay", "Sumanth", "Satyamm")  // new Array() is a constructor function.
+// console.log(names);
+// console.log(names.sort());// alphbetical order
+// console.log(names.reverse()); // reverse that order
+
+// const numbers = [15,2,1,8,3]
+// console.log(numbers.sort()); //for numbers the sort is not working properly so use following below way to do that using arrow function but text the old way goes well
+
+
+
+
+const numbers = [15,2,1,8,3]
+
+// console.log(numbers.sort((a,b) => a - b)); // ascending 
+// console.log(numbers.sort((a,b) => b - a)); // descending 
+
+// console.log(numbers.indexOf(2)); // printing index of the 2 in the array numbers
+
+// console.log(numbers.lastIndexOf(2));//It searches an array for a value.
+
+//                                      //returns the index of the last occurrence of that value.
+
+//                                       //If the value is not found, it returns -1.
+
+
+// console.log(numbers.includes(3));   // tell whether it is their or not
+
+// console.log(numbers.slice(1,3));// prints these indexed and btwns
+
