0464215a98db9f5365ea6d35af5c8814a9a383b0
Tarun Swami <152185804+tarunstack@users.noreply.github.com>
2026-03-11 17:10:44 +0530

    Create file59.js

---
+++ file59.js
@@ -0,0 +1,23 @@
+// student = {
+//     name:"Nikhil",
+//     display:function(){
+//         console.log(this.name)
+//     }
+// }
+// student.display()
+
+// student = {
+//   name: "Nikhil",
+//   display: () => {
+//     console.log(this.name);
+//   },
+// };
+// student.display();
+
+student = {
+  name: "Nikhil",
+  display() {
+    console.log(this.name);
+  },
+};
+student.display();
